3ba7f6c943a41f7d6ac1504dabfaf261484d2a2e
“Batzaya” <tsunade83@gmail.com>
2020-07-08 14:54:35 +0800

    Second Challenge Solution

--- app.js
+++ app.js
@@ -15,6 +15,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+app.get("/", function(req, res){
+    res.render("home",{startingContent:homeStartingContent});
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
