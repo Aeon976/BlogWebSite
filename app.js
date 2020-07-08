078ca57ca293393cfecf2ce24037618e642e6163
“Batzaya” <tsunade83@gmail.com>
2020-07-08 15:17:37 +0800

    3rd Challenge Solution

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 app.get("/", function(req, res){
-    res.render("home",{startingContent:homeStartingContent});
+    res.render("home", { startingContent:homeStartingContent });
 });
 
 app.listen(3000, function() {
