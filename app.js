c300abe6fc5304d86f6350dbba36b9e4dad9215a
“Batzaya” <tsunade83@gmail.com>
2020-07-11 20:10:46 +0800

    17th Challenge Solution

--- app.js
+++ app.js
@@ -47,7 +47,16 @@ app.post("/compose", function(req, res){
 });
 
 app.get("/posts/:postName", function(req, res){
-    console.log(req.params.postName);
+  const requestedTitle = req.params.postName;
+  
+  posts.forEach(function(post) {
+      const storedTitle = post.title;
+
+      if (storedTitle === requestedTitle) {
+        console.log("Match found!");
+      }
+  });
+
 });
 
 app.listen(3000, function() {
